78bf93898462c5f1a3472a01d1f3ab61de0d7527
bncprado <bncprado@gmail.com>
2025-01-13 09:41:06 +0000

    control flow (advanced) and file org

---
+++ Computer Science/9. Challenges/Control Flow (advanced)/3. 3. Always False.py
@@ -0,0 +1,27 @@
+""""
+There are some situations that you normally want to avoid when programming using conditional statements. One example is a contradiction. This occurs when your condition will always be false no matter what value you pass into it. Let's create an example of a function that contains a contradiction. It will contain a few steps:
+
+1. Define the function to accept a single parameter called num
+2. Use a combination of <, > and and to create a contradiction in an if statement.
+3. If the condition is true, return True, otherwise return False. The trick here is that because we've written a contradiction, the condition should never be true, so we should expect to always return False.
+
+Coding question
+
+Create a function named always_false() that has one parameter named num.
+
+Using an if statement, your variable num, and the operators >, and <, make it so your function will return False no matter what number is stored in num.
+
+An if statement that is always false is called a contradiction. You will rarely want to do this while programming, but it is important to realize it is possible to do this.
+"""
+# Write your always_false function here:
+def always_false(num):
+  if num >= num and num <= num:
+    return False
+
+# Uncomment these function calls to test your always_false function:
+print(always_false(0))
+# should print False
+print(always_false(-1))
+# should print False
+print(always_false(1))
+# should print False
