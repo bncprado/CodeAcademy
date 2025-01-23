715a9d6cb38269ac10b76928d9432f7cf8248b86
bncprado <bncprado@gmail.com>
2025-01-23 10:59:50 +0000

    reversed list finished

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
-my_list = [4, 8, 10, 11, 12]
-my_list2 = [4, 8, 1, 11, 1]
+my_list = [1,2,3]
+my_list2 = [3,2,1]
 
 matching = []
 
 matching.insert(0,[1,2,])
 matching.insert(1,[1,2])
 
-print(matching)
+for i in range(len(my_list)):
+  if my_list[i]==my_list2[-1-i]:
+    print(True)
+  else:
+    print(False)
+
