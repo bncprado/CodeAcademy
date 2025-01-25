4763918574dc405dec419a7895e27d7c350966b5
bncprado <bncprado@gmail.com>
2025-01-25 12:32:55 +0000

    initial commit

---
+++ Computer Science/2. Programming in Python on Your Computer/2. The Boredless Tourist.py
@@ -0,0 +1,7 @@
+""""
+1. We here at The Boredless Tourist believe that mistakes should be easily corrected, so we keep all of our code version controlled using git.
+Start by running git init in the terminal.
+
+2. We'll be doing most of our coding in "2. The Boredless Tourist.py" so we want to make sure that we are tracking that in git.
+Add it to git's staging area.
+"""
