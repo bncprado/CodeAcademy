d4bcbb98f1248fca58b02e0a1c422740b303a2b2
bncprado <bncprado@gmail.com>
2025-01-25 12:31:45 +0000

    file rename

---
+++ Computer Science/2. Programming in Python on Your Computer/The Boredless Tourist.py
@@ -0,0 +1,9 @@
+""""
+1. We here at The Boredless Tourist believe that mistakes should be easily corrected, so we keep all of our code version controlled using git.
+
+Start by running git init in the terminal.
+
+2. We'll be doing most of our coding in script.py so we want to make sure that we are tracking that in git.
+
+Add script.py to git's staging area.
+"""
