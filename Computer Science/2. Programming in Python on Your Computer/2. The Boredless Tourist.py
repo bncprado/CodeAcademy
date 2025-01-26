ece835ff8c9f94352d0ec9df1ef0f51a39797c28
bncprado <bncprado@gmail.com>
2025-01-26 20:16:54 +0000

    Added logic to find traveler destinations and convert to internal data

--- Computer Science/2. Programming in Python on Your Computer/2. The Boredless Tourist.py
+++ Computer Science/2. Programming in Python on Your Computer/2. The Boredless Tourist.py
@@ -19,7 +19,7 @@ Create a list with the following destinations and save it into a variable called
 “São Paulo, Brazil”
 “Cairo, Egypt”
 """
-destinations = ["Paris, France", "Shangai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 
 """"
 5. And let's define a test traveler to see how our functionality is working so far.
@@ -72,21 +72,46 @@ print(get_destination_index("Paris, France"))
 13.What happens if we call get_destination_index() with a destination not in our destinations list?
 Try it now: call get_destination_index() with the argument “Hyderabad, India”. What happens?
 """
-print(get_destination_index("Hyderabad, India"))
+# print(get_destination_index("Hyderabad, India"))
 
 """"
 14. If you used the .index() method to get the index of a destination from the list, you'll notice that calling get_destination_index() with data that is missing from our destinations list will raise a ValueError.
 Don't add any logic to avoid triggering this ValueError, it's going to be useful for us in the future.
 
 15. Now let's define a function called get_traveler_location().
-get_traveler_location() is going to take a single parameter, traveler.
+"get_traveler_location()" is going to take a single parameter, traveler.
 
 16. In the body of get_traveler_location(), access the traveler's destination string and save it into traveler_destination.
 
 17. Use traveler_destination along with get_destination_index() to retrieve the index of the destination where the traveler is. Save the index of the traveler's destination into the variable traveler_destination_index.
+
+18. Make get_traveler_location() return the destination index of the traveler by returning traveler_destination_index.
+
 """
 
 def get_traveler_location(traveler):
-  traveler_destination = traveler
+  traveler_destination = traveler[1]
+  traveler_destination_index = get_destination_index(traveler_destination)
+  return traveler_destination_index
 
+""""
+19. Create a variable called test_destination_index. Save the results of calling get_traveler_location() with our test_traveler.
+"""
 
+test_destination_index = get_traveler_location(test_traveler)
+
+""""
+20. Print out test_destination_index.
+"""
+print(test_destination_index)
+
+""""
+21. Save your code and run it by calling python3 script.py in the terminal.
+Is the test_destination_index you created equal to 1?
+
+22. Let's save our work to the git tracker. Add script.py to the git index with git add.
+
+23. And commit your changes with the message
+"Added logic to find traveler destinations and convert to internal data"
+
+"""
