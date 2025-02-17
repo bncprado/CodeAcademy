f4adb15e97e1b1c475bdf7b7748d48db8b0eef5c
bncprado <bncprado@gmail.com>
2025-02-17 09:31:25 +0000

    testing split with multi line text

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
-fruit = "watermelon"
+when_you_are_old = \
+"""When you are old and grey and full of sleep,
+And nodding by the fire, take down this book,
+And slowly read, and dream of the soft look
+Your eyes had once, and of their shadows deep;
 
-# for i in range(len(fruit)):  
-#   print (i)
-  
-# print(25/0)
-# for i in range(10):
-#   if i%2==0:
-#     print (i)
+How many loved your moments of glad grace,
+And loved your beauty with love false or true,
+But one man loved the pilgrim soul in you,
+And loved the sorrows of your changing face;
 
-# for i in len(fruit):
-#   print(i)
+And bending down beside the glowing bars,
+Murmur, a little sadly, how Love fled
+And paced upon the mountains overhead
+And hid his face amid a crowd of stars."""
+
+split = when_you_are_old.split("\n")
+
+print(split)
