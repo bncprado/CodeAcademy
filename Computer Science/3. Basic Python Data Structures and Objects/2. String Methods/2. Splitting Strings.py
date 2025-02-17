5fb0c6fa6d55742693f0301bd684a24a2c5c518e
bncprado <bncprado@gmail.com>
2025-02-17 09:44:40 +0000

    more notes on splits

--- Computer Science/3. Basic Python Data Structures and Objects/2. String Methods/2. Splitting Strings.py
+++ Computer Science/3. Basic Python Data Structures and Objects/2. String Methods/2. Splitting Strings.py
@@ -10,6 +10,18 @@ line_one = "The sky has given over"
 line_one_words = line_one.split() #default value will split anything separated by a blank space
 #=> created a list with every word of line_one variable
 
+print(line_one_words)
+
+""""
+Split method will not only split by the typed argument, but also REMOVE the argument from the given string
+"""
+
+word = "banana"
+
+word_split = word.split("a")
+#=>['b', 'n', 'n', '']
+
+print(word_split)
 
 
 
