77b0a4aeea610ce110ebaf49e93a61461bdb9faf
bncprado <bncprado@gmail.com>
2025-02-17 08:46:02 +0000

    string methods complete, including review

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 fruit = "watermelon"
 
-print(i for i in fruit)
+# for i in range(len(fruit)):  
+#   print (i)
   
+# print(25/0)
+# for i in range(10):
+#   if i%2==0:
+#     print (i)
+
+# for i in len(fruit):
+#   print(i)
