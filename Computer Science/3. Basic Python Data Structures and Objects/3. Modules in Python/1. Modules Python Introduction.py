f0eb89c45b806fed08c0cb4f5a87cacb94c42e61
bncprado <bncprado@gmail.com>
2025-02-21 13:57:27 +0000

    starting modules in python

---
+++ Computer Science/3. Basic Python Data Structures and Objects/3. Modules in Python/1. Modules Python Introduction.py
@@ -0,0 +1,15 @@
+""""
+1. import the datetime type from the datetime library.
+"""
+from datetime import datetime
+
+""""
+2. Create a variable current_time and set it equal to datetime.now().
+"""
+current_time = datetime.now()
+
+""""
+3. Print out current_time.
+"""
+
+print(current_time)
---
+++ Computer Science/3. Basic Python Data Structures and Objects/3. Modules in Python/2. Modules Python Random.py
@@ -0,0 +1,10 @@
+# Import random below:
+
+
+# Create random_list below:
+
+
+# Create randomer_number below:
+
+
+# Print randomer_number below:
