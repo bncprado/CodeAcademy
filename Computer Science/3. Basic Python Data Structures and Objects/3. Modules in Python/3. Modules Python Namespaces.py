0144f4431b7152bdf7005ee84f1a562b755aba72
bncprado <bncprado@gmail.com>
2025-03-08 09:34:18 +0000

    more about libraries in python

---
+++ Computer Science/3. Basic Python Data Structures and Objects/3. Modules in Python/3. Modules Python Namespaces.py
@@ -0,0 +1,14 @@
+# 3
+
+# Add your code below:
+from matplotlib import pyplot as plt
+
+import random
+
+numbers_a = range(1,13)
+
+numbers_b = random.sample(range(0,1000),12)
+
+plt.plot(numbers_a,numbers_b)
+
+plt.show()
---
+++ Computer Science/3. Basic Python Data Structures and Objects/3. Modules in Python/4. Modules Python Decimals.py
@@ -0,0 +1,9 @@
+# Import Decimal below:
+from decimal import Decimal
+
+# Fix the floating point math below:
+two_decimal_points = 0.2 + 0.69
+print(f"{two_decimal_points:.2f}")
+four_decimal_points = 0.53 * 0.65
+print(f"{four_decimal_points:.4f}")
+
