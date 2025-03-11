a478941ff8425fa47bf83bca2e8dde68f588532a
bncprado <bncprado@gmail.com>
2025-03-11 15:04:33 +0000

    more dictionaries learning

---
+++ Computer Science/3. Basic Python Data Structures and Objects/4. Dictionaries/2. Using Dictionaries/6. Get All Values.py
@@ -0,0 +1,8 @@
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+total_exercises = 0
+
+for values in num_exercises.values():
+  total_exercises += values
+
+print(total_exercises)
