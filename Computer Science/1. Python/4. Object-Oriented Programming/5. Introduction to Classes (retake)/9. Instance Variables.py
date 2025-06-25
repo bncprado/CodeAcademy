ff30edab0eb68dec09f64eb8f6fc726d40709d18
bncprado <bncprado@gmail.com>
2025-06-25 21:19:05 +0100

    instance variables

--- Computer Science/1. Python/4. Object-Oriented Programming/5. Introduction to Classes (retake)/9. Instance Variables.py
+++ Computer Science/1. Python/4. Object-Oriented Programming/5. Introduction to Classes (retake)/9. Instance Variables.py
@@ -4,3 +4,10 @@ class Store:
 alternative_rocks = Store()
 isabelles_ices = Store()
 
+alternative_rocks.store_name = "Alternative Rocks"
+isabelles_ices.store_name = "Isabelle's Ices"
+
+
+print(f"The name of the shop one is {alternative_rocks.store_name}")
+
+print(f"The name of the shop one is {isabelles_ices.store_name}")
